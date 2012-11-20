6d7dd418b1c87431074e5365eb788d4c64731fd0
Gijs Nijholt <gijs.nijholt@gmail.com>
2012-11-20 22:59:44 +0100

    Adding libs

---
+++ apikey.js
@@ -0,0 +1,5 @@
+// enter your API key here
+tomtom.apiKey = "aztb7b4z64svu56d3wbvqd35";
+
+if (!tomtom.apiKey)
+	alert("Please edit apikey.js and enter your API key. The examples will not function until an API key has been entered.");
